b903a0c21b426ff6072f6a4a1ecb78cfd1e20bb2
Mitanshubad <119037058+Mitanshubad@users.noreply.github.com>
2024-01-01 20:08:52 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -2,15 +2,6 @@ import axios from 'axios';
 
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
-// const options = {
-//   params: {
-//     maxResults: 50,
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-//   },
-// };
 const options = {
   method: 'GET',
   url: 'https://youtube-v31.p.rapidapi.com/search',
